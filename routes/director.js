17f062e9800d016f0c0bb2f3572e7f4e4644f4b5
unknown <emreozer@gmail.com>
2018-03-18 22:52:42 +0300

    api/directors getAllDirectors ve getDirectorById eklendi

--- routes/director.js
+++ routes/director.js
@@ -1,8 +1,109 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const router = express.Router();
 
 // Models
 const Director = require('../models/Director');
+
+router.get('/', (req, res, next) => {
+    const promise = Director.aggregate([
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+    ]);
+
+    promise.then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.get('/:director_id', (req, res, next) => {
+    const promise = Director.aggregate([
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+    ]);
+
+    promise.then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
 router.post('/', (req, res, next) => {
     const director = new Director(req.body);
